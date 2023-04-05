2c93337dd5a32c568e794cea75ab408d15f165b0
Hao Guo <49539832+guohao1995@users.noreply.github.com>
2023-04-05 12:27:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # Python program to demonstrate 
 # main() function 
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# print("Hello")
+
+# # Defining main function
+# def main():
+#     print("hey there")
 
 
-print("Hello")
-
-# Defining main function
-def main():
-    print("hey there")
-
-
-# Using the special variable 
-# __name__
-if __name__=="__main__":
-    main()
+# # Using the special variable 
+# # __name__
+# if __name__=="__main__":
+#     main()
